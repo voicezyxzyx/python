e3674bc6caac3ee4af42f821348a57f6e00af5e1
voicezyxzyx <450538542.com>
2018-08-21 10:49:30 +0800

    file

--- 简单客户端与服务端通信/client.py
+++
@@ -1,8 +0,0 @@
-import socket
-udpClient=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-while True:
-    data=input("请输入数据：")
-    udpClient.sendto(data.encode("utf-8"),('192.168.221.1',8900))
-
-    # info=udpClient,recv(1024).decode("utf-8")
-    # print("服务器说：",info)
