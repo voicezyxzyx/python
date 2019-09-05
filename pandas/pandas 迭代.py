15c47863bba12579aeb9ccb05c4f72ea2bfb2984
voicezyxzyx <450538542@qq.com>
2019-09-05 15:22:07 +0800

    Create pandas 迭代.py

---
+++ pandas/pandas 迭代.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+N=20
+
+df = pd.DataFrame({
+    'A': pd.date_range(start='2016-01-01',periods=N,freq='D'),
+    'x': np.linspace(0,stop=N-1,num=N),
+    'y': np.random.rand(N),
+    'C': np.random.choice(['Low','Medium','High'],N).tolist(),
+    'D': np.random.normal(100, 10, size=(N)).tolist()
+    })
+
+for col in df:
+   print (col)
